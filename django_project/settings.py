bb599eea89138e7d5432ae0c019f56010bceaae2
VitaliiFedin <vitalii.fedinn@gmail.com>
2022-12-16 19:22:40 +0200

    init commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -84,7 +84,10 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": env.dj_db_url("CLEARDB_DATABASE_URL")
+    "default": {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
